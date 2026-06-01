e52f9c0765597dc80d7ce4c3404042cae4fb6eb3
XMG-001 <1691256006@qq.com>
2026-06-01 17:12:59 +0800

    Update jx-ga4-monitor.js

--- jx-ga4-monitor.js
+++ jx-ga4-monitor.js
@@ -11,6 +11,11 @@
         ignoreURL: ['checkout.shopify.com', 'pay.google.com', 'js.stripe.com', 'paypal.com'],
         sandboxURL: ['web-pixels'],
         isTop: window.self === window.top,
+        isSameOrigin() {
+            if (this.isTop) return true;
+            const host = (() => { try { return window.top.location.hostname } catch { return new URL(document.referrer || '').hostname } })();
+            return host === window.location.hostname;
+        },
         isSandbox() { return !this.isTop && this.sandboxURL.some(d => window.location.href.includes(d)); },
         logLoaded(mode) {
             console.log(
@@ -20,10 +25,11 @@
             );
         },
         check() {
+            if (this.isTop) this.logLoaded();
             const { href, hostname } = window.location;
             if (!href || href === 'about:blank' || this.ignoreURL.some(d => hostname.includes(d) || href.includes(d))) return false;
-            if (this.isTop) this.logLoaded();
-            return this.isTop || this.isSandbox();
+            const valid = this.isTop || (this.isSameOrigin() || this.isSandbox());
+            return valid;
         }
     };
     if (!ENV.check()) return;
